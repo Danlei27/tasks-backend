264b7e7d3d05fb90f9f36afab68e7410fa26f8a0
danlei santos <danleisantos@hotmails.com>
2020-12-05 11:36:22 -0300

    Middlewares e db com knex

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const db = require('./config/db')
+const consign = require('consign')
 
-app.use(bodyParser.json())
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
-app.get('/', (req, res) =>{
-    res.status(200).send('Meu Backend!')
-})
+app.db = db
 
 app.listen(3000, () =>{
     console.log('Backend executando...')
